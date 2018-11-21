6775921fdab5449f0caddf440288d988edffe208
t-kojima <t-kojima@skullware.net>
2018-11-21 23:44:42 +0900

    pass: test case

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -3,6 +3,20 @@ require './lib/convert_hash_syntax'
 
 class ConvertHashSyntaxText < Minitest::Test
   def test_convert_hash_syntax
-    assert_equal '{}', convert_hash_syntax('{}')
+    old_syntax = <<~TEXT
+    {
+      :name => 'Alice',
+      :age => 20,
+      :gender => :female
+    }
+    TEXT
+    expected = <<~TEXT
+    {
+      name: 'Alice',
+      age: 20,
+      gender: :female
+    }
+    TEXT
+    assert_equal expected, convert_hash_syntax(old_syntax)
   end
 end
